9fee35460da736580fa43d58976bbf0e48dc5222
Philipp Wambach <philippwambach@gmail.com>
2015-01-18 12:27:45 +0100

    first example

--- main.js
+++ main.js
@@ -15,6 +15,9 @@ var VIEW_ANGLE = 45,
 var renderer = new THREE.WebGLRenderer({alpha: true, antialias: true });
 renderer.setClearColor(0xeeeeee, 1);
 renderer.setSize(WIDTH, HEIGHT);
+renderer.shadowMapEnabled = true;
+renderer.shadowMapSoft = true;
+renderer.shadowMapType = THREE.PCFSoftShadowMap;
 
 // Scene
 var scene = new THREE.Scene();
@@ -26,51 +29,129 @@ camera.position.y = 250;
 camera.lookAt(new THREE.Vector3(0,0,0));
 scene.add(camera);
 
-// Light
-var pointLight = new THREE.PointLight( 0xFFFFFF );
-pointLight.position.x = 10;
-pointLight.position.y = 50;
-pointLight.position.z = 130;
-scene.add(pointLight);
+// Point Light
+var spotLight = new THREE.SpotLight( 0xcccccc );
+spotLight.position.x = 50;
+spotLight.position.y = 150;
+spotLight.position.z = 200;
+spotLight.castShadow = true;
+spotLight.shadowDarkness = 0.1;
+spotLight.angle = 360;
+scene.add(spotLight);
+var pointLightHelper = new THREE.PointLightHelper( spotLight, 10 );
+scene.add( pointLightHelper );
 
+//Ambient Lihgt
+var light = new THREE.AmbientLight( 0x444444 ); // soft white light
+scene.add( light );
 
 // Helper
-scene.add(new THREE.AxisHelper(100));
-scene.add(new THREE.GridHelper(100,10));
+var axisHelper = new THREE.AxisHelper(100)
+var gridHelper = new THREE.GridHelper(100,10)
+scene.add(axisHelper);
+scene.add(gridHelper);
 
-
-
+// Controls
+var controls = new THREE.OrbitControls(camera);
 
 // Sphere
-var sphereMaterial = new THREE.MeshLambertMaterial({color: 0xEEEEEE});
-var radius = 50, segments = 32, rings = 32;
+var sphereMaterial = new THREE.MeshLambertMaterial({color: 0x2222ee, side: THREE.DoubleSide});
+var radius = 10, segments = 32, rings = 32;
 var sphere = new THREE.Mesh(
    	new THREE.SphereGeometry(radius, segments, rings),
    	sphereMaterial
    );
+sphere.position.y = radius;
+sphere.castShadow = true;
 scene.add(sphere);
 
-
-
-/*var skyboxGeometry = new THREE.CubeGeometry(500, 500, 500, 1, 1, 1, null, true);
-var skyboxMaterial = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, side: THREE.BackSide });
-var skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
-scene.add(skybox);*/
-
-
-
+// Ground
+var planeMaterial = new THREE.MeshBasicMaterial( {color: 0xeeeeee, side: THREE.FrontSide} );
+var plane = new THREE.Mesh( new THREE.PlaneGeometry( 1000, 1000, 12 ), planeMaterial );
+plane.rotation.x = Math.PI / 2 + Math.PI;
+plane.receiveShadow = true;
+scene.add(plane);
 
 
 
+// Boxes
+var numberOfBoxes = 6;
+var boxWidth = 20;
+var boxHeight = 50;
+var boxDepth = 20;
+var boxes = [];
+var spreadArea = 300;
+var boxMaterial = new THREE.MeshLambertMaterial({color: 0xeeeeee, side: THREE.FrontSide});
 
-function render() {
-	sphere.rotation.y += 0.01;
-    renderer.render(scene, camera); 
-    requestAnimationFrame(render);
+for(var i = 0; i < numberOfBoxes; i++) {
+	var box = new THREE.Mesh(new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth, 1, 1, 1), boxMaterial)
+	box.position.set(
+		(Math.random()*spreadArea)-spreadArea/2,
+		boxHeight/2,
+		(Math.random()*spreadArea)-spreadArea/2);
+	box.castShadow = true;
+	boxes.push(box);
+	scene.add(box);
+};
+
+
+
+
+
+
+
+var lightMove = 0;
+function moveLight(){
+	lightMove += 0.01;
+	if(lightMove > 360){
+		lightMove = 0;
+	}
+	spotLight.position.x = Math.sin(lightMove) * 200;
 }
 
 
 
-
+// render loop
+function render() {
+	// sphere.rotation.y += 0.01;
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+    moveLight();
+}
 $('#container').append(renderer.domElement); 
 render();
+
+	
+
+
+
+var moveForward = function(){
+	sphere.position.
+};
+
+var turnLeft = function(){
+	
+};
+
+var turnRight = function(){
+	
+};
+
+var moveBackward = function(){
+	
+};
+
+
+// interactions
+$('body').on('keydown', function(e){
+	switch(e.keyCode){
+		case 38: moveForward(); break;
+		case 37: turnLeft(); break;
+		case 39: turnRight(); break;
+		case 40: moveBackward(); break;
+		case 32: 
+			axisHelper.visible = !axisHelper.visible;
+			gridHelper.visible = !gridHelper.visible;
+			break;
+	}
+});
