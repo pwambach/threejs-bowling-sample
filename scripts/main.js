fe7253fe6f3830e2110789de024435993f441fdd
Philipp Wambach <philippwambach@gmail.com>
2015-03-22 12:43:53 +0100

    Update 2015-03-22T11:43:50.205Z

--- scripts/main.js
+++ scripts/main.js
@@ -43,11 +43,12 @@ var Keyboard = function(){
 	//set up Physijs
 	Physijs.scripts.worker = 'physijs_worker.js';
 	Physijs.scripts.ammo = 'ammo.js';
+    var simulate = true;
 
 
 	// WebGL Renderer
 	var renderer = new THREE.WebGLRenderer({antialias: true });
-	renderer.setClearColor(0xeeeeee, 1);
+	renderer.setClearColor(0x6faf46, 1);
 	renderer.setSize(WIDTH, HEIGHT);
 	renderer.shadowMapEnabled = true;
 	renderer.shadowMapSoft = true;
@@ -55,15 +56,27 @@ var Keyboard = function(){
 
 	// Physijs Scene
 	scene = new Physijs.Scene();
-	scene.setGravity(new THREE.Vector3(0,-100,0));
+	scene.setGravity(new THREE.Vector3(0,-800,0));
 
 	// Camera
-	var camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
-	camera.position.z = 550;
-	camera.position.x = 300;
-	camera.position.y = 250;
-	camera.lookAt(new THREE.Vector3(0,0,0));
+/*	var perspectiveCamera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+	perspectiveCamera.position.z = 550;
+	perspectiveCamera.position.x = 300;
+	perspectiveCamera.position.y = 250;
+	perspectiveCamera.lookAt(new THREE.Vector3(0,0,0));
+	scene.add(perspectiveCamera);*/
+
+	//Orthographic Camera
+	var aspect = window.innerWidth / window.innerHeight;
+	var d = 400;
+	var camera = new THREE.OrthographicCamera( - d * aspect, d * aspect, d, - d, 1, 2500 );
+	//var camera = new THREE.OrthographicCamera( 1000 / -2, 1000 / 2, 800 / 2, 800 / -2, 1, 1000 );
+	camera.position.set( 800, 400, 800 );
+	camera.lookAt( new THREE.Vector3(0,0,0) ); // or the origin
 	scene.add(camera);
+
+/*	var cameraHelper = new THREE.CameraHelper(camera);
+	scene.add(cameraHelper);*/
 
 
 	// Keyboard
@@ -75,19 +88,21 @@ var Keyboard = function(){
 	controls.noKeys = true;
 
 
-	var spotLight = new THREE.PointLight(0xcccccc);
+	/*var spotLight = new THREE.PointLight(0xcccccc);
     spotLight.position.y = 1000;
     spotLight.angle = Math.PI/2;
     spotLight.shadowDarkness = 0.1;
     scene.add(spotLight);
 
     var pointLightHelper = new THREE.PointLightHelper(spotLight, 100);
-    scene.add(pointLightHelper);
+    scene.add(pointLightHelper);*/
 
 
 	// Render loop
 	function render() {
-		scene.simulate(); // run physics
+		if(simulate){
+			scene.simulate(); // run physics
+		}
 	    renderer.render(scene, camera);
         requestAnimationFrame(render);
 	}
@@ -105,6 +120,16 @@ var Keyboard = function(){
 	};
 	window.app = app;
 
+
+	$('body').on('keydown', function(e){
+		//'s' key for simulate
+		if(e.keyCode === 83){
+			simulate = !simulate;
+			console.log("simulation: ", simulate);
+		}
+	});
+
+
 })(this);
 
 
@@ -116,20 +141,22 @@ var Keyboard = function(){
 
 (function(){
 
-	var color = 0x2222ee,
-		radius = 10,
-		segments = 32,
-		rings = 32;
+	var color = 0x61cbe3,
+		radius = 15,
+		segments = 24,
+		rings = 24;
 		mass = 10;
 
 	// Sphere
-	var sphereMaterial = Physijs.createMaterial(new THREE.MeshLambertMaterial({color: color, side: THREE.FrontSide}), 1, 1);
+	var sphereMaterial = Physijs.createMaterial(new THREE.MeshBasicMaterial({color: color, side: THREE.FrontSide}), 1, 1);
 	var sphereGeometry = new THREE.SphereGeometry(radius, segments, rings)
 	
 	var sphere = new Physijs.SphereMesh(sphereGeometry, sphereMaterial, mass);
+	sphere.setLinearFactor(new THREE.Vector3( 0, 0, 0 )); // only move on X and Z axis
 
 	sphere.position.y = 30;
-	sphere.position.z = 200;
+	sphere.position.z = -200;
+	sphere.position.x = 200;
 	sphere.castShadow = true;
 	//sphere.setDamping(1, 1);
 	
@@ -160,7 +187,7 @@ var Keyboard = function(){
 		*/
 
 
-	    var speed = 4000;
+/*	    var speed = 4000;
 	    var force = new THREE.Vector3(0, 0, 0);
 
 	    if (app.keyboard.isUP()) {
@@ -183,7 +210,7 @@ var Keyboard = function(){
 	            preventJump = false;
 	        }, 500);
 	        sphere.applyCentralImpulse(new THREE.Vector3(0, speed, 0));
-	    }
+	    }*/
 
 	    if(sphere.position.y < -50){
 	    	sphere.position.y = 20;
@@ -194,8 +221,8 @@ var Keyboard = function(){
 	    	sphere.__dirtyPosition = true;
 	    }
 
-	    sphere.applyCentralForce(force);
-	    requestAnimationFrame(movePlayer);
+	    //sphere.applyCentralForce(force);
+	    //requestAnimationFrame(movePlayer);
 	};
 	movePlayer();
 
@@ -207,11 +234,12 @@ var Keyboard = function(){
     // Axis Helper
     var axisHelper = new THREE.AxisHelper(100);
     axisHelper.position.y = 20;
+    axisHelper.visible = false
     app.scene.add(axisHelper);
 
     // Grid Helper
-    var gridHelper = new THREE.GridHelper(100, 10);
-    // app.scene.add(gridHelper);
+    /*var gridHelper = new THREE.GridHelper(100, 10);
+    app.scene.add(gridHelper);*/
 
 
     // sho/hide helpers
@@ -241,53 +269,91 @@ var Keyboard = function(){
 (function(){
 
 	// Ground
-	var groundColor = 0xffffff,
+	var groundColor = 0xf5ffaf,
 		sizeX = 200,
 		sizeZ = 600;
 		sizeY = 10;
-	var planeMaterial = new THREE.MeshLambertMaterial( {color: groundColor, side: THREE.FrontSide} );
+	var planeMaterial = Physijs.createMaterial(new THREE.MeshLambertMaterial( {color: groundColor, side: THREE.FrontSide} ), 1,0.5);
+	
 	var planeGeometry = new THREE.BoxGeometry( sizeX, sizeY, sizeZ, 1,1,1 );
+	var planeGeometry2 = new THREE.BoxGeometry( sizeX, sizeY, 200, 1,1,1 );
+	var planeGeometry3 = new THREE.BoxGeometry( sizeX, sizeY, sizeZ, 1,1,1 );
+	
 	var plane = new Physijs.BoxMesh(planeGeometry, planeMaterial, 0 );
+	var plane2 = new Physijs.BoxMesh(planeGeometry2, planeMaterial, 0 );
+	var plane3 = new Physijs.BoxMesh(planeGeometry3, planeMaterial, 0 );
 	plane.position.y = -5;
+	plane.position.x = sizeX;
+
+	plane2.position.y = -5;
+	plane2.position.x = 0;
+	plane2.position.z = 200;
+
+	plane3.position.y = -5;
+	plane3.position.x = -200;
+
 	plane.receiveShadow = true;
+	plane2.receiveShadow = true;
+	plane3.receiveShadow = true;
 	app.ground = plane;
 	app.scene.add(plane);
-
-
-	var planeMaterial2 = new THREE.MeshBasicMaterial( {color: 0xcccccc, side: THREE.FrontSide} );
-	var planeGeometry2 = new THREE.PlaneGeometry( 200, 600, 1,1 );
-	var plane2 = new THREE.Mesh(planeGeometry2, planeMaterial2);
-	plane2.rotation.x = Math.PI / -2;
-	plane2.position.y = -20;
-	plane2.receiveShadow = true;
 	app.scene.add(plane2);
+	app.scene.add(plane3);
+
+
+
+	var makeBorder = function(x, z, w, h)  {
+	    var border = new Physijs.BoxMesh(
+	      new THREE.CubeGeometry(w, 100, h),
+	      Physijs.createMaterial(
+	        new THREE.MeshBasicMaterial({color: 0x000000}), 0.2, 1.0    
+	      ),
+	      0 
+	    );
+	    border.position.set(x, 50, z);
+	    border.visible = false;
+	    return border;
+  	};
+
+  	app.scene.add(makeBorder(-305, 0, 10, 600));
+  	app.scene.add(makeBorder(305, 0, 10, 600));
+  	app.scene.add(makeBorder(95, -100, 10, 400));
+  	app.scene.add(makeBorder(-95, -100, 10, 400));
+  	app.scene.add(makeBorder(0, 305, 600, 10));
+  	app.scene.add(makeBorder(0, 95, 200, 10));
+  	app.scene.add(makeBorder(0, -305, 600, 10));
 
 
 
 	// Boxes
-	var boxColor = 0xeeeeee,
+	var boxColor = 0xe6f2e5,
 		numberOfBoxes = 6,
 		boxWidth = 20,
 		boxHeight = 40,
 		boxDepth = 20;
+		offsetX = -200;
+		offsetZ = -200;
 	var boxes = [];
 
 	var boxMaterial = new THREE.MeshLambertMaterial({color: boxColor, side: THREE.FrontSide});
 	var boxGeometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth, 1, 1, 1);
 	
 	for(var i = 0; i < numberOfBoxes; i++) {
-		var box = new Physijs.BoxMesh(boxGeometry, boxMaterial, 100)
+		var box = new Physijs.BoxMesh(boxGeometry, boxMaterial, 25)
 		box.castShadow = true;
+		box.receiveShadow = true;
+		box.position.set(0,boxHeight*i+(boxHeight/2), 0);
 		boxes.push(box);
+
 		
 	};
 
-	boxes[0].position.set(0,boxHeight/2,-200);
-	boxes[1].position.set(-30,boxHeight/2,-200);
-	boxes[2].position.set(30,boxHeight/2,-200);
-	boxes[3].position.set(-15,boxHeight/2+boxHeight,-200);
-	boxes[4].position.set(15,boxHeight/2+boxHeight,-200);
-	boxes[5].position.set(0,boxHeight/2+(2*boxHeight),-200);
+	boxes[0].position.set(0 + offsetX,boxHeight/2,offsetZ);
+	boxes[1].position.set(-30 + offsetX,boxHeight/2,offsetZ);
+	boxes[2].position.set(30 + offsetX,boxHeight/2,offsetZ);
+	boxes[3].position.set(-15 + offsetX,boxHeight/2+boxHeight,offsetZ);
+	boxes[4].position.set(15 + offsetX,boxHeight/2+boxHeight,offsetZ);
+	boxes[5].position.set(0 + offsetX,boxHeight/2+(2*boxHeight),offsetZ);
 
 	boxes.forEach(function(box){
 		app.scene.add(box);
@@ -296,7 +362,7 @@ var Keyboard = function(){
 })();
 (function() {
 
-    var impulseScalar = 80;
+    var impulseScalar = 40;
 
     var $container = app.container;
     var character = app.character;
@@ -305,7 +371,6 @@ var Keyboard = function(){
     var mouse = new THREE.Vector2();
     var powerPoint;
     var powerLineGeometry = new THREE.Geometry();
-    powerLineGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
     powerLineGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
     var powerLineMaterial = new THREE.LineBasicMaterial({
         color: 0x5555ff,
@@ -321,7 +386,7 @@ var Keyboard = function(){
 
 
     var material = new THREE.MeshBasicMaterial( {color: 0xcccccc, side: THREE.FrontSide} );
-    var geometry = new THREE.PlaneGeometry( 1000, 1000, 1,1 );
+    var geometry = new THREE.PlaneGeometry( 3000, 3000, 1,1 );
     var plane = new THREE.Mesh(geometry, material);
     plane.rotation.x = Math.PI / -2;
     plane.position.y = 5;
@@ -391,13 +456,28 @@ var Keyboard = function(){
 (function() {
 
     //Ambient Light
-    var light = new THREE.AmbientLight(0x555555); // soft white light
-    app.scene.add(light);
+    var light = new THREE.AmbientLight(0x333333); // soft white light
+    // app.scene.add(light);
 
+    // Directional Light
+    var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
+    directionalLight.position.set( 200, 300, 200 );
+    directionalLight.castShadow = true;
+    directionalLight.shadowDarkness = 0.4;
+    directionalLight.shadowCameraNear = 10;
+    directionalLight.shadowCameraFar = 800;
+    directionalLight.shadowCameraVisible = false;
+    directionalLight.shadowCameraFov = 60;
+    app.scene.add( directionalLight );
+
+        //Light Helper
+/*    var directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 10);
+    app.scene.add(directionalLightHelper);*/
 
     // Point Light
-    var spotLight = new THREE.SpotLight(0xcccccc);
+/*    var spotLight = new THREE.SpotLight(0xcccccc);
     spotLight.position.y = 550;
+    spotLight.position.x = 200;
     spotLight.position.z = 200;
     spotLight.castShadow = true;
     spotLight.shadowDarkness = 0.4;
@@ -406,11 +486,11 @@ var Keyboard = function(){
     spotLight.angle = Math.PI / 2;
     spotLight.shadowCameraVisible = false;
     spotLight.shadowCameraFov = 60;
-    app.scene.add(spotLight);
+    app.scene.add(spotLight);*/
 
     //Light Helper
-    var pointLightHelper = new THREE.PointLightHelper(spotLight, 10);
-    app.scene.add(pointLightHelper);
+/*    var pointLightHelper = new THREE.PointLightHelper(spotLight, 10);
+    app.scene.add(pointLightHelper);*/
 
 
     // Light Move
@@ -423,6 +503,6 @@ var Keyboard = function(){
         spotLight.position.x = Math.sin(lightMove) * 200;
         //requestAnimationFrame(moveLight);
     }
-    moveLight();
+    //moveLight();
 
 })();
